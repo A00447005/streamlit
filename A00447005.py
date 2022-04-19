da76e2b689b00f7462f474c7c11beac0e50674a0
Viren V Varasadiya <Viren.Vinubhai.Varasadiya@smu.ca>
2022-04-18 22:31:36 -0300

    Initial commit

---
+++ A00447005.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import requests
+import streamlit as st
+
+
+days = st.slider('Days', 1, 365, 90)
+
+currency = st.radio(
+     "currency",
+     ('CAD', 'USD', 'INR'))
+
+BASE_URL = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency="
+
+API_URL = BASE_URL + currency +"&days=" + str(no_of_days)+"&interval=daily"
+
+#GET DATA 
+req = requests.get(API_URL)
+
+if req.status_code == 200:
+    data = req.json()['prices']
+    df = pd.DataFrame(data=data, columns=['Date', 'Price'])
+    df['Date'] = pd.to_datetime(df['Date'], unit='ms')
+    df = df.set_index('Date')
+    st.line_chart(df)
+    st.text('Average price during this time was '+ str(df['Price'].mean()) + ' '+ currency)
